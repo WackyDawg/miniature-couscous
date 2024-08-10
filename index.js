6e9a59e56b4c2d4cfcf051507da7a4e05fa659e1
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-08-10 20:30:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const path = require('path');
-const fs = require('fs');
-
-const app = express();
-const PORT = 3000;
-const screenshotPath = path.join(__dirname, 'example.png');
-
-// Serve the screenshot at /screenshot
-app.get('/screenshot', (req, res) => {
-  if (fs.existsSync(screenshotPath)) {
-    res.sendFile(screenshotPath);
-  } else {
-    res.status(404).send('Screenshot not found.');
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const fs = require('fs')
 
 // Function to simulate a delay
 function delay(time) {
@@ -51,7 +34,6 @@ function delay(time) {
     // Add a short delay between clicks using the delay function
     await delay(500); // 500ms delay, adjust as needed
   }  
-  await page.screenshot({ path: screenshotPath });
 
   console.log(`Screenshot saved and available at http://localhost:${PORT}/screenshot`);
 
